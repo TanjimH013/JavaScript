33e63e6695a256d76015a784da31e1b39edbb97f
Tanjim Hasan <tanjim13hasan@gmail.com>
2023-10-07 16:59:19 +0600

    update

--- Practice/script.js
+++ Practice/script.js
@@ -29,19 +29,28 @@ function golfScore(par, strokes) {
     return names[0];
   } else if (strokes <= par - 2) {
     return names[1];
-  } else if (strokes - 1) {
+  } else if (strokes == par - 1) {
     return names[2];
-  } else if (strokes === par) {
+  } else if (strokes == par) {
     return names[3];
-  } else if (strokes === par + 1) {
+  } else if (strokes == par + 1) {
     return names[4];
-  } else if (strokes === par + 2) {
+  } else if (strokes == par + 2) {
     return names[5];
-  } else if (strokes === par + 2) {
+  } else if (strokes >= 3 + 1) {
     return names[6];
   }
-
-  // Only change code above this line
 }
-golfScore(6, 1);
+console.log(golfScore(4, 1));
+console.log(golfScore(4, 2));
 console.log(golfScore(5, 4));
+golfScore(4, 1);
+console.log(golfScore(4, 2));
+console.log(golfScore(5, 2));
+console.log(golfScore(4, 3));
+console.log(golfScore(3, 4));
+console.log(golfScore(1, 1));
+console.log(golfScore(5, 5));
+console.log(golfScore(4, 5));
+console.log(golfScore(4, 6));
+console.log(golfScore(4, 7));
