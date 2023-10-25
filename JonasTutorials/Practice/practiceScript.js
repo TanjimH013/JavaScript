3110a08079a7331e6482efc6ff2c11cbbd4997e3
Tanjim Hasan <tanjim13hasan@gmail.com>
2023-10-25 11:37:11 +0600

    revers loop practice

--- JonasTutorials/Practice/practiceScript.js
+++ JonasTutorials/Practice/practiceScript.js
@@ -48,3 +48,22 @@ const reverseString = function (str) {
 };
 
 console.log(reverseString("'B' 'A' 'N' 'G' 'L' 'A' 'D' 'E' 'S' 'H'"));
+
+const reverseString2 = function (str) {
+  let newString2 = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString2 = newString2 + str[i];
+  }
+  return newString2;
+};
+
+console.log(reverseString2("'T' 'I' 'G' 'E' ''R"));
+
+const reverseString3 = function (str) {
+  let newString3 = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString3 = newString3 + str[i];
+  }
+  return newString3;
+};
+console.log(reverseString3("'C' 'H' 'I' 'T' 'T' 'A' 'G' 'O' 'N'"));
