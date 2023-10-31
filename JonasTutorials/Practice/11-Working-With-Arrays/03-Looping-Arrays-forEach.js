8584370250bc56e3665e2585017de7a17d783578
Tanjim Hasan <tanjim13hasan@gmail.com>
2023-10-31 15:06:04 +0600

    Looping arrays forEach

---
+++ JonasTutorials/Practice/11-Working-With-Arrays/03-Looping-Arrays-forEach.js
@@ -0,0 +1,121 @@
+'use strict';
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+/////////////////////////////////////////////////
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+for (const movement of movements) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('----forEach Method----');
+/*it'll pass in the current element of the array as an argument */
+
+movements.forEach(function (i) {
+  if (i > 0) {
+    console.log(`You deposited ${i}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(i)}`);
+  }
+});
+// every time it receive element as an argument
+/*
+0: function(200) 
+1: function(450) 
+2: function(400)
+3:  ...
+*/
+
+console.log('--- entries method in for of loop ---');
+const iteration = [200, 450, -400, 3000, -650, -130, 70, 1300];
+for (const [i, iterate] of iteration.entries()) {
+  if (iterate > 0) {
+    console.log(`Iterate ${i + 1}: You deposited ${iterate}`);
+  } else {
+    console.log(`Iterate ${i + 1}: YOu withdrew ${Math.abs(iterate)}`);
+  }
+}
+
+console.log('-----forEach----');
+
+iteration.forEach(function (iterate, i, arr) {
+  if (iterate > 0) {
+    console.log(`Iterate ${i + 1}: You deposited ${iterate} `);
+  } else {
+    console.log(`Iterate ${i + 1}: You withdrew ${Math.abs(iterate)}`);
+  }
+});
