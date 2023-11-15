f66a65ae70dae365f47445e07c1f7f8b28fcf630
Tanjim Hasan <tanjim13hasan@gmail.com>
2023-11-15 13:37:56 +0600

    LWS Array

--- LWSumit-Tutorials/script.js
+++ LWSumit-Tutorials/script.js
@@ -2,3 +2,14 @@
 let myNumber = 32;
 //myNumber.toString(10);
 console.log(myNumber.toString(32));
+
+const city = [" Chittagong", " Dhaka", " Rajshahi", " Khulna ", " Sylhet"];
+
+document.getElementById("demo").innerHTML = city;
+
+console.log(typeof city);
+console.log(city.length);
+
+for (i = 0; i < city.length; i++) {
+  console.log(city[i]);
+}
