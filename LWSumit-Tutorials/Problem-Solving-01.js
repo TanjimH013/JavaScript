f7778d995d5c8d016afec0a79f697b11d4927d4a
Tanjim Hasan <tanjim13hasan@gmail.com>
2023-11-18 08:59:52 +0600

    Random number between min and max(both included)

--- LWSumit-Tutorials/Problem-Solving-01.js
+++ LWSumit-Tutorials/Problem-Solving-01.js
@@ -1,1 +1,10 @@
 "Use Strict";
+console.log("--- Random number between min and max(both included ---)");
+//Random number between min and max(both included)
+
+const getRndInteger = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+console.log(getRndInteger(1, 6));
+
+//
