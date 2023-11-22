3d97e798113e1567e851a18cb79ad709386991ea
Tanjim Hasan <tanjim13hasan@gmail.com>
2023-11-22 10:28:30 +0600

    LWS-Bitwis-

---
+++ LWSumit-Tutorials/LWS-Bitwis-Operators.js
@@ -0,0 +1,12 @@
+"use strict";
+//Decimal to Binary conversion
+console.log((488005).toString(2));
+
+//Binary to Decimal conversion
+console.log((0b1110111001001000101).toString(10));
+
+//Decimal to Octal conversion
+console.log((488005).toString(8));
+
+//Decimal to Hexadecimal
+console.log((488005).toString(16));
