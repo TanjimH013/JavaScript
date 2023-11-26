48d95e3449239fcb27e0046770caaa8a5d1fc9d1
Tanjim Hasan <tanjim13hasan@gmail.com>
2023-11-26 12:18:11 +0600

    LWS-Class updated

--- LWSumit-Tutorials/LWS-Class.js
+++ LWSumit-Tutorials/LWS-Class.js
@@ -19,4 +19,71 @@ const personTow = new person("Pabel", "30", 4526987125);
 
 personTow.insertCard();
 
-console.log();
+console.log("---------------");
+
+class Ticket {
+  constructor(
+    coachClass,
+    sit,
+    passengerTotal,
+    passengerName,
+    nid,
+    cellNum,
+    pick,
+    drop,
+    price
+  ) {
+    this.coachClass = coachClass;
+    this.sit = sit;
+    this.passengerTotal = passengerTotal;
+    this.passengerName = passengerName;
+    this.nid = nid;
+    this.cellNum = cellNum;
+    this.pick = pick;
+    this.drop = drop;
+    this.price = price;
+  }
+
+  detailPassengerPrint() {
+    console.table(`
+    Class:${this.coachClass},
+    Sit serial : ${this.sit}.
+    
+    Passenger Total: ${this.passengerTotal},
+    Passenger Name: ${this.passengerName},
+    NID: ${this.nid},
+    Contact Number: ${this.cellNum}.
+    
+    Depart From: ${this.pick},
+    Drop Area: ${this.drop}.
+    
+    Fare:${this.passengerTotal * this.price} only.
+    `);
+  }
+}
+const TicketOne = new Ticket(
+  "Business",
+  "D1-D3,E1",
+  4,
+  "Rayhan Mahmud",
+  "42265453384",
+  "+880 1521486527",
+  "Chittagong",
+  "Shirajgonj",
+  1500
+);
+TicketOne.detailPassengerPrint();
+
+const TicketTow = new Ticket(
+  "Business",
+  "B2,B3",
+  2,
+  "Rayhan Mahmud",
+  "42265453384",
+  "+880 1521486527",
+  "Chittagong",
+  "Dhaka",
+  1150
+);
+
+TicketTow.detailPassengerPrint();
