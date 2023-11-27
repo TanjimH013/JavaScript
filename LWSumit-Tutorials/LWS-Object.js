737f6b978676b7ab0a9621711f1c533eb1edf0d6
Tanjim Hasan <tanjim13hasan@gmail.com>
2023-11-27 15:54:25 +0600

    LWS-Make-Object-Iterable

--- LWSumit-Tutorials/LWS-Object.js
+++ LWSumit-Tutorials/LWS-Object.js
@@ -13,3 +13,30 @@ const Person = function (firstName, lastName, age, eyeColor) {
 console.log(Person("Tanjim", "Hasan", "30", "Dark Brown"));
 console.log(Person("Rayhan", "Mahmud", "30", "Black"));
 console.log(Person("Afzalur", "Rahman", "30", "Black"));
+
+//Object iterables
+
+const myNumber = {};
+
+//make it iterable
+
+myNumber[Symbol.iterator] = function () {
+  let n = 0;
+  let done = false;
+  return {
+    next() {
+      n += 10;
+      if (n == 100) {
+        done = true;
+      }
+      return {
+        value: n,
+        done: done,
+      };
+    },
+  };
+};
+
+for (let num of myNumber) {
+  console.log(num);
+}
