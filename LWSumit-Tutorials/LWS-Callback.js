69f03b6d31add6bfb4b044eff2689b1b6732d1db
Tanjim Hasan <tanjim13hasan@gmail.com>
2023-12-02 18:36:15 +0600

    LWS-Callback-Updated

--- LWSumit-Tutorials/LWS-Callback.js
+++ LWSumit-Tutorials/LWS-Callback.js
@@ -6,12 +6,25 @@ const display = function (some) {
 
 const calculate = function (num1, num2) {
   let sum = num1 + num2;
-  //display(sum); //2nd way
+  display(sum); //2nd way
   return sum;
 };
 
-// calculate(10,15);
+calculate(10, 15);
 
-let result = calculate(10, 15);
+// let result = calculate(10, 15);
 
-display(result); // primary way
+// display(result); // primary way
+
+// Callback Function
+
+const display2 = function (some) {
+  console.log(some);
+};
+
+const calculate2 = function (num1, num2, callback) {
+  let sum = num1 + num2;
+  callback(sum);
+};
+
+calculate2(10, 15, display2);
