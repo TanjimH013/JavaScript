05ccd9670cbded7e8ea225065fe62d291c32e296
Tanjim Hasan <tanjim13hasan@gmail.com>
2023-12-06 10:26:20 +0600

    LWS-DOM-Form

--- LWSumit-Tutorials/LWS-DOM.js
+++ LWSumit-Tutorials/LWS-DOM.js
@@ -1,1 +1,12 @@
 "Use Strict";
+const validateForm = function () {
+  const form = document.forms["myForm"];
+
+  const value = form["fname"].value;
+
+  if (value === "") {
+    alert("Name must be filled out");
+    console.log(value);
+    return false;
+  }
+};
