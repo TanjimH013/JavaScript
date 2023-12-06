9a0012c8ba4afe66bac23d12b476cc5c5a8ff7f5
Tanjim Hasan <tanjim13hasan@gmail.com>
2023-12-06 10:53:13 +0600

    LWS-DOM-CSS

--- LWSumit-Tutorials/LWS-DOM.js
+++ LWSumit-Tutorials/LWS-DOM.js
@@ -10,3 +10,13 @@ const validateForm = function () {
     return false;
   }
 };
+
+//handel css
+
+setTimeout(function () {
+  const p = document.querySelector(".paragraph");
+
+  p.style.color = "blue";
+  p.style.fontSize = "2.8em";
+  p.style.fontWeight = "600";
+}, 2000);
