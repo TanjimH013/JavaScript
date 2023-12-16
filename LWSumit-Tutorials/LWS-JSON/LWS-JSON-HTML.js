cf1526dda07699ac0ededdcb5d40929a1718d79b
tanjim <tanjim13hasan@gmail.com>
2023-12-16 23:41:01 +0600

    LWS-JSON-HTML

---
+++ LWSumit-Tutorials/LWS-JSON/LWS-JSON-HTML.js
@@ -0,0 +1,11 @@
+const change_myselect = async function(sel) {
+    // const dbParam = JSON.stringify({table:sel,limit:20});
+    
+    const response =await fetch("https://www.w3school.com/js/json_demo_html_table.php"{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: "x=" + JSON.stringify(),
+    })
+  }
