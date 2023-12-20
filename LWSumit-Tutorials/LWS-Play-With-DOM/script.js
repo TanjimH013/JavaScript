d76d06846f2fda00fe1a4a036d2148d3bc6ea883
tanjim <tanjim13hasan@gmail.com>
2023-12-20 11:28:59 +0600

    LWS-DOM-Traversing

--- LWSumit-Tutorials/LWS-Play-With-DOM/script.js
+++ LWSumit-Tutorials/LWS-Play-With-DOM/script.js
@@ -1,5 +1,18 @@
 'Use Strict';
-let itemLi = document.querySelectorAll('.item');
-for(let element of itemLi){
-    element.style.color ='red'
+// let itemLi = document.querySelectorAll('.item');
+// for(let element of itemLi){
+//     element.style.color ='red'
+// } 
+
+let headerElement = document.querySelector('#header');
+headerElement.innerHTML='To-Do Apps'
+headerElement.style.color = 'blue'
+headerElement.style.fontSize = '3.5em'
+
+// let lastItem = document.querySelector('.item:nth-child(2)')
+// lastItem.style.color='red'
+
+let itemAll = document.querySelectorAll('.item:childe')
+for(element of itemAll){
+    element.style.color = 'red'
 }
