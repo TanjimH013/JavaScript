0f777e182f4523407f300e6a294dba2cab384a9b
tanjim <tanjim13hasan@gmail.com>
2023-12-20 15:11:29 +0600

    LWS-Traversing -DOM-Hierarchy

--- LWSumit-Tutorials/LWS-Play-With-DOM/script.js
+++ LWSumit-Tutorials/LWS-Play-With-DOM/script.js
@@ -1,21 +1,33 @@
 'Use Strict';
-let itemLi = document.querySelectorAll('.item');
-for(let element of itemLi){
-    element.style.color ='red'
-} 
+// let itemLi = document.querySelectorAll('.item');
+// for(let element of itemLi){
+//     element.style.color ='red'
+// } 
 
-let headerElement = document.querySelector('#header');
-headerElement.innerHTML='To-Do Apps'
-headerElement.style.color = 'blue'
-headerElement.style.fontSize = '3.5em'
+// let headerElement = document.querySelector('#header');
+// headerElement.innerHTML='To-Do Apps'
+// headerElement.style.color = 'blue'
+// headerElement.style.fontSize = '3.5em'
 
-// let lastItem = document.querySelector('.item:nth-child(2)')
-// lastItem.style.color='red'
+// // let lastItem = document.querySelector('.item:nth-child(2)')
+// // lastItem.style.color='red'
 
-let itemAll = document.querySelectorAll('.item:childe')
-for(element of itemAll){
-    element.style.color = 'red'
-}
+// let itemAll = document.querySelectorAll('.item:childe')
+// for(element of itemAll){
+//     element.style.color = 'red'
+// }
 
 
 
+// parent/child relation
+const grandparent = document.querySelector('.todo-list');
+const childrenOne = grandparent.querySelector('.item')
+childrenOne.style.color='red'
+
+const childrenTow = childrenOne.nextElementSibling;
+childrenTow.style.color = 'green'
+
+const childrenThree = childrenTow.nextElementSibling;
+childrenThree.style.color ='blue'
+
+console.log(children);
