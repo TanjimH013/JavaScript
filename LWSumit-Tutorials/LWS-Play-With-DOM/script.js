e6d8837d45441d9007cc10391fda83133c828bc3
tanjim <tanjim13hasan@gmail.com>
2023-12-20 11:55:36 +0600

    Trying tracking

--- LWSumit-Tutorials/LWS-Play-With-DOM/script.js
+++ LWSumit-Tutorials/LWS-Play-With-DOM/script.js
@@ -1,8 +1,8 @@
 'Use Strict';
-// let itemLi = document.querySelectorAll('.item');
-// for(let element of itemLi){
-//     element.style.color ='red'
-// } 
+let itemLi = document.querySelectorAll('.item');
+for(let element of itemLi){
+    element.style.color ='red'
+} 
 
 let headerElement = document.querySelector('#header');
 headerElement.innerHTML='To-Do Apps'
